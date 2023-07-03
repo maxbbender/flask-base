a0e5911892229889c8c1fb655dac8a9b5ce0cfae
Max Bender <maxbbender@gmail.com>
2023-07-03 06:43:29 +0000

    updates to base

--- app/__init__.py
+++ app/__init__.py
@@ -13,16 +13,12 @@ from config import config
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 from flask_login import LoginManager, current_user
-
-# from flask_mail import Mail, email_dispatched
-# from flask_wtf.csrf import CSRFProtect
+from flask_wtf.csrf import CSRFProtect
 
 db = SQLAlchemy()
 login_manager = LoginManager()
+csrf = CSRFProtect()
 admin = Admin(name='MyApp', template_mode='bootstrap3')
-
-# mail = Mail()
-# csrf = CSRFProtect()
 
 
 def createApp(configName):
@@ -36,82 +32,27 @@ def createApp(configName):
     from app.models import User
     admin.add_view(ModelView(User, db.session))
 
-    # mail.init_app(app)
-    # csrf.init_app(app)
-    # login_manager.init_app(app)
+    csrf.init_app(app)
+    login_manager.init_app(app)
     
 
-    # login_manager.login_view = '/login'
+    login_manager.login_view = '/login'
 
-    # @login_manager.user_loader
-    # def load_user(user_id):
-    #     from app.models.user import User
-    #     return db.session.query(User).filter_by(id=user_id).first()
+    @login_manager.user_loader
+    def load_user(user_id):
+        from app.models.user import User
+        return db.session.query(User).filter_by(id=user_id).first()
 
-    # @login_manager.unauthorized_handler
-    # def unauthorized():
-    #     app.logger.warning('User unauthorized : URL(%s)' % (request.url))
-    #     # do stuff
-    #     return redirect(url_for('users.login'))
-
-    # if app.config['BRAINTREE_ENV'] == 'sandbox':
-    #     braintree.Configuration.configure(braintree.Environment.Sandbox,
-    #                                       merchant_id=app.config[
-    #                                           'BRAINTREE_MERCHANT_ID'],
-    #                                       public_key=app.config[
-    #                                           'BRAINTREE_PUBLIC_KEY'],
-    #                                       private_key=app.config['BRAINTREE_PRIVATE_KEY'])
-    # elif app.config['BRAINTREE_ENV'] == 'production':
-    #     braintree.Configuration.configure(braintree.Environment.Production,
-    #                                       merchant_id=app.config[
-    #                                           'BRAINTREE_MERCHANT_ID'],
-    #                                       public_key=app.config[
-    #                                           'BRAINTREE_PUBLIC_KEY'],
-    #                                       private_key=app.config['BRAINTREE_PRIVATE_KEY'])
-    # else:
-    #     app.logger.error('Unknown BRAINTREE_ENV : %s' %
-    #                      app.config['BRAINTREE_ENV'])
+    @login_manager.unauthorized_handler
+    def unauthorized():
+        app.logger.warning('User unauthorized : URL(%s)' % (request.url))
+        # do stuff
+        return redirect(url_for('users.login'))
 
     # Blueprints
     from app.blueprints.main import main as main_blueprint
 
     app.register_blueprint(main_blueprint)
-  
-    # @app.context_processor
-    # def insert_login_form():
-    #     if current_user.is_authenticated:
-    #         passwordChangeForm = PasswordChangeForm()
-    #         avatarChangeForm = ProfilePictureForm()
-    #         dmForm = DirectMessageForm()
-    #         # messages, notifications = current_user.unreadNotifications()
-    #         notifications = current_user.getNotifications()
-    #         messages = current_user.getMessageNotifications()
-    #         houses = current_user.houseList
-
-    #         numUnviewedNotifications = current_user.getUnreadNotificationCount()
-    #         numUnviewedMessages = current_user.getUnreadMessageNotificationCount()
-
-    #         return dict(passwordChangeForm=passwordChangeForm,
-    #                     avatarChangeForm=avatarChangeForm,
-    #                     notifications=notifications,
-    #                     numUnviewedNotifications=numUnviewedNotifications,
-    #                     numUnviewedMessages=numUnviewedMessages,
-    #                     notificationMessages=messages,
-    #                     platformReportForm=PlatformReportForm(),
-    #                     DirectMessageForm=DirectMessageForm(),
-    #                     houses=houses,
-    #                     dmForm=dmForm,
-    #                     contactForm=ContactForm())
-    #     else:
-    #         login_form = LoginForm()
-    #         dmForm = DirectMessageForm()
-
-    #         return dict(login_form=login_form,
-    #                     platformReportForm=PlatformReportForm(),
-    #                     dmForm=dmForm,
-    #                     contactForm=ContactForm())
-
-    # import nexnest.admin
 
     def format_datetime(value, format='human'):
         if format == 'human':
@@ -126,35 +67,14 @@ def createApp(configName):
     app.jinja_env.filters['format_datetime'] = format_datetime
     app.jinja_env.filters['format_date'] = format_date
 
-    # @app.context_processor
-    # def override_url_for():
-    #     return dict(url_for=dated_url_for)
+    CSRF Error Handler
+    from flask_wtf.csrf import CSRFError
+    from flask import abort, flash
 
-    # def dated_url_for(endpoint, **values):
-    #     if endpoint == 'static':
-    #         filename = values.get('filename', None)
-    #         if filename:
-    #             file_path = os.path.join(app.root_path,
-    #                                      endpoint, filename)
-    #             values['q'] = int(os.stat(file_path).st_mtime)
-    #     return url_for(endpoint, **values)
-
-    # CSRF Error Handler
-    # from flask_wtf.csrf import CSRFError
-    # from flask import abort, flash
-
-    # @app.errorhandler(CSRFError)
-    # def handle_csrf_error(e):
-    #     app.logger.warning('CSRF error : %r' % e)
-    #     flash('CSRF Error. Please go back, refresh the page and try again.', 'warning')
-    #     return abort(405)
+    @app.errorhandler(CSRFError)
+    def handle_csrf_error(e):
+        app.logger.warning('CSRF error : %r' % e)
+        flash('CSRF Error. Please go back, refresh the page and try again.', 'warning')
+        return abort(405)
 
     return app
-
-
-# def logEmailDispatch(message, app):
-#     app.logger.debug('Email Sent! Subject %s | Text %s' %
-#                      (message.subject, message.html))
-
-
-# email_dispatched.connect(logEmailDispatch)
--- config.py
+++ config.py
@@ -6,26 +6,7 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     FLASK_ADMIN_SWATCH = 'cerulean'
-
-    # UPLOAD_FOLDER = basedir + '/nexnest/uploads'
-    # ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif', 'pdf'])
-    # TRAP_BAD_REQUEST_ERRORS = True
-
-    # ADMINS = ['max@nexnest.com']
-
-    
     SECURITY_PASSWORD_SALT = os.environ.get('SECURITY_PASSWORD_SALT')
-    # GOOGLE_MAPS_KEY = os.environ.get('GOOGLE_MAPS_KEY')
-    # GOOGLE_CAPTCHA_SECRET = os.environ.get('GOOGLE_CAPTCHA_SECRET')
-    # FLASK_CONFIG = os.environ.get('FLASK_CONFIG')
-    # BRAINTREE_MERCHANT_ID = os.environ.get('BRAINTREE_MERCHANT_ID')
-    # BRAINTREE_PUBLIC_KEY = os.environ.get('BRAINTREE_PUBLIC_KEY')
-    # BRAINTREE_PRIVATE_KEY = os.environ.get('BRAINTREE_PRIVATE_KEY')
-    # BRAINTREE_ENV = os.environ.get('BRAINTREE_ENV') or 'sandbox'
-    # SLACK_LOG_URL = os.environ.get(
-    #     'SLACK_LOG_URL') or 'https://hooks.slack.com/services/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'
-    # SLACK_WARNING_URL = os.environ.get('SLACK_WARNING_URL')
-    # SLACK_ERROR_URL = os.environ.get('SLACK_ERROR_URL')
 
     @staticmethod
     def init_app(app):
@@ -46,25 +27,17 @@ class DevelopmentConfig(Config):
     @classmethod
     def init_app(cls, app):
         Config.init_app(app)
-        # import logging
-        # from slack_log_handler import SlackLogHandler
-        # slackHandler = SlackLogHandler(Config.SLACK_LOG_URL)
-        # slackHandler.setLevel(logging.WARNING)
-        # app.logger.addHandler(slackHandler)
 
     DEBUG = True
-    # MAIL_SERVER = 'mail.nexnest.com'
-    # MAIL_PORT = 587
-    # MAIL_USE_TLS = True
-    # MAIL_USE_SSL = False
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    MAIL_SUPPRESS_SEND = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
 
 
 class TestingConfig(Config):
+    @classmethod
+    def init_app(cls, app):
+        Config.init_app(app)
+
     TESTING = True
     MAIL_SUPPRESS_SEND = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
@@ -76,35 +49,39 @@ class ProductionConfig(Config):
     def init_app(cls, app):
         Config.init_app(app)
 
-        # app.logger.handlers = []
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    DEBUG = False
+    TESTING = False
 
-        # import logging
-        # from logging.handlers import RotatingFileHandler
-        # from slack_log_handler import SlackLogHandler
+    # app.logger.handlers = []
 
-        # # Slack Warning
-        # slackWarningHandler = SlackLogHandler(
-        #     Config.SLACK_WARNING_URL) or Config.SLACK_LOG_URL
-        # slackWarningHandler.setLevel(logging.WARNING)
-        # app.logger.addHandler(slackWarningHandler)
+    # import logging
+    # from logging.handlers import RotatingFileHandler
+    # from slack_log_handler import SlackLogHandler
 
-        # # Slack Error
-        # slackErrHandler = SlackLogHandler(
-        #     Config.SLACK_ERROR_URL) or Config.SLACK_LOG_URL
-        # slackErrHandler.setLevel(logging.ERROR)
-        # app.logger.addHandler(slackErrHandler)
+    # # Slack Warning
+    # slackWarningHandler = SlackLogHandler(
+    #     Config.SLACK_WARNING_URL) or Config.SLACK_LOG_URL
+    # slackWarningHandler.setLevel(logging.WARNING)
+    # app.logger.addHandler(slackWarningHandler)
 
-        # # Debug Handler
-        # debugHandler = RotatingFileHandler(
-        #     '/var/log/nexnest/debug.log', maxBytes=10000, backupCount=5)
-        # debugHandler.setLevel(logging.DEBUG)
-        # app.logger.addHandler(debugHandler)
+    # # Slack Error
+    # slackErrHandler = SlackLogHandler(
+    #     Config.SLACK_ERROR_URL) or Config.SLACK_LOG_URL
+    # slackErrHandler.setLevel(logging.ERROR)
+    # app.logger.addHandler(slackErrHandler)
 
-        # # Info Handler
-        # infoHandler = RotatingFileHandler(
-        #     '/var/log/nexnest/info.log', maxBytes=10000, backupCount=5)
-        # infoHandler.setLevel(logging.INFO)
-        # app.logger.addHandler(infoHandler)
+    # # Debug Handler
+    # debugHandler = RotatingFileHandler(
+    #     '/var/log/nexnest/debug.log', maxBytes=10000, backupCount=5)
+    # debugHandler.setLevel(logging.DEBUG)
+    # app.logger.addHandler(debugHandler)
+
+    # # Info Handler
+    # infoHandler = RotatingFileHandler(
+    #     '/var/log/nexnest/info.log', maxBytes=10000, backupCount=5)
+    # infoHandler.setLevel(logging.INFO)
+    # app.logger.addHandler(infoHandler)
 
     # MAIL SERVER CONFIG
     # MAIL_SERVER = 'mail.nexnest.com'
@@ -112,11 +89,7 @@ class ProductionConfig(Config):
     # MAIL_USE_TLS = True
     # MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     # MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
-    DEBUG = False
-    TESTING = False
-
-
+   
 class UnixConfig(ProductionConfig):
     @classmethod
     def init_app(cls, app):
@@ -125,7 +98,7 @@ class UnixConfig(ProductionConfig):
         # log to syslog
         import logging
         from logging.handlers import SysLogHandler
-        syslog_handler = SysLogHandler(address='/var/log/nexnest.log')
+        syslog_handler = SysLogHandler(address='/var/log/app.log')
         syslog_handler.setLevel(logging.WARNING)
         app.logger.addHandler(syslog_handler)
 
